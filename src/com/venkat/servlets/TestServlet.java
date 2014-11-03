9e4fa2ad707366efbf3501b539e0b907eb828278
Vamsi Krishna Aitha <ivamsi2001@gmail.com>
2014-11-04 01:04:12 +0530

    Changed Test Servlet
    Changed Test Servlet

--- src/com/venkat/servlets/TestServlet.java
+++ src/com/venkat/servlets/TestServlet.java
@@ -13,6 +13,7 @@ public class TestServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Changed Test Servlet");
 		doPost(req, resp);
 	}
 	
